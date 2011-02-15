42cfd61058b1004bdcc09399b830af30b49bc011
james.cook <james.cook@usma.edu>
2011-02-15 11:41:57 -0500

    Improving comments

--- src/Unsafe/Unique/Tag.hs
+++ src/Unsafe/Unique/Tag.hs
@@ -12,16 +12,19 @@ import Unsafe.Coerce
 import Control.Monad.Primitive
 import Control.Monad
 
--- |A super-special ad-hoc GADT-like thing. 'Tag's can be generated in any 
--- primitive monad (but only tags from the same one can be compared).  Every
--- tag is equal to itself and to no other.  The 'GOrdering' class allows 
--- rediscovery of a tag's phantom type, so that 'Tag's and values of type
--- @'DSum' ('Tag' s)@ can be tested for equality even when their types are not 
--- known to be equal.
+-- |The 'Tag' type is like an ad-hoc GADT allowing runtime creation of new 
+-- constructors.  Specifically, it is like a GADT \"enumeration\" with one
+-- phantom type.
+-- 
+-- A 'Tag' constructor can be generated in any primitive monad (but only tags
+-- from the same one can be compared).  Every tag is equal to itself and to 
+-- no other.  The 'GOrdering' class allows rediscovery of a tag's phantom type,
+-- so that 'Tag's and values of type @'DSum' ('Tag' s)@ can be tested for
+-- equality even when their types are not known to be equal.
 --
--- A 'Tag' uses a 'Uniq' as a witness of type equality, which is sound as
--- long as the 'Uniq' is truly unique and only one 'Tag' is ever constructed
--- from any given 'Uniq'.  The type of 'newTag' enforces these conditions.
+-- 'Tag' uses a 'Uniq' as a witness of type equality, which is sound as long
+-- as the 'Uniq' is truly unique and only one 'Tag' is ever constructed from
+-- any given 'Uniq'.  The type of 'newTag' enforces these conditions.
 -- 'veryUnsafeMkTag' provides a way for adventurous (or malicious!) users to 
 -- assert that they know better than the type system.
 newtype Tag s a = Tag (Uniq s) deriving (Eq, Ord)
@@ -42,9 +45,7 @@ instance GCompare (Tag s) where
 -- it is, but I have not thought through it very deeply and certainly have not
 -- proved it.)
 newTag :: PrimMonad m => m (Tag (PrimState m) a)
-newTag = do
-    u <- getUniq
-    return (Tag u)
+newTag = liftM Tag getUniq
 
 -- |Very dangerous! This is essentially a deferred 'unsafeCoerce': by creating
 -- a tag with this function, the user accepts responsibility for ensuring 
